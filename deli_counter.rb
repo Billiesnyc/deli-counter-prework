8eed68721042ef6eee52a408813283f7718e2ebe
Billie Simmons <billiesnyc@gmail.com>
2018-08-15 13:20:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli)
     else 
       formatted_line = []
         deli.each do |name|
-          formatted_line.push(name.index + 1 + ". " + name.join(" "))
+          formatted_line.push(deli.index + 1 + ". " + name.join(" "))
         end
         puts "The line is currently:" + formatted_line
     end
@@ -16,7 +16,7 @@ end
 def take_a_number(deli, name)
   if deli.length == 0 
     deli.push(name)
-    puts "Welcome, #{name}. You are number #{name.index + 1} in line."
+    puts "Welcome, #{name}. You are number #{deli.index + 1} in line."
   else
     deli.push(name)
     "Welcome, #{name}. You are number #{name.index + 1} in line."
