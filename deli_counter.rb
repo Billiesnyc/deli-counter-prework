cb1d135836bc1088fa9f457856b8543396dfbc9c
Billie Simmons <billiesnyc@gmail.com>
2018-08-15 13:08:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(deli)
     else 
       formatted_line = []
       deli.each do |name|
-        formatted_line.push(name.index + 1 + ". " + name.join)
+        formatted_line.push(name.index + 1 + ". " + name.join(" "))
       puts "The line is currently:" + deli.each do |name| 
         puts name.index + 1 + ". " + name + " "
       end
