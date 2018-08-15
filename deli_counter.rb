48d89b012d67130b6294c1f6c9cefc019a3c623b
Billie Simmons <billiesnyc@gmail.com>
2018-08-15 13:04:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(deli)
     if deli.length == 0 
       puts "The line is currently empty."
     else 
-      puts "The line is currently:" + deli.each {|name| puts name.index + 1 + ". " + name}
+      puts "The line is currently:" + deli.each do |name| 
+        puts name.index + 1 + ". " + name
     end
 
 end
